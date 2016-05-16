c7a9865b064cbbcf1f50c12f779c8c9eb7a05228
slavik <slavikok2@gmail.com>
2016-05-16 15:19:42 +0300

    fix pagination

--- src/main/webapp/plugins/table-elements.js
+++ src/main/webapp/plugins/table-elements.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
 	     "autoWidth": false,
 		"ajax": $.fn.dataTable.pipeline({
 			url: '../../restful/api/features',
-			pages: 5 // number of pages to cache
 		}),
 		"columns": [
 						{"data": "id", "title":'ID'},
@@ -138,119 +137,36 @@ $(document).ready(function(){
     });
 
 });
-
-
-///////////////////////////
-
 //
 // Pipelining function for DataTables. To be used to the `ajax` option of DataTables
 //
 $.fn.dataTable.pipeline = function ( opts ) {
 	// Configuration options
 	var conf = $.extend( {
-		pages: 5,     // number of pages to cache
 		url: '',      // script url
 		data: null,   // function or object with parameters to send to the server
 		              // matching how `ajax.data` works in DataTables
 		method: 'GET' // Ajax HTTP method
 	}, opts );
 
-	// Private variables for storing the cache
-	var cacheLower = -1;
-	var cacheUpper = null;
-	var cacheLastRequest = null;
-	var cacheLastJson = null;
 
 	return function ( request, drawCallback, settings ) {
-		var ajax          = false;
-		var requestStart  = request.start;
-		var drawStart     = request.start;
-		var requestLength = request.length;
-		var requestEnd    = requestStart + requestLength;
-		
-		if ( settings.clearCache ) {
-			// API requested that the cache be cleared
-			ajax = true;
-			settings.clearCache = false;
-		}
-		else if ( cacheLower < 0 || requestStart < cacheLower || requestEnd > cacheUpper ) {
-			// outside cached data - need to make a request
-			ajax = true;
-		}
-		else if ( JSON.stringify( request.order )   !== JSON.stringify( cacheLastRequest.order ) ||
-		          JSON.stringify( request.columns ) !== JSON.stringify( cacheLastRequest.columns ) ||
-		          JSON.stringify( request.search )  !== JSON.stringify( cacheLastRequest.search )
-		) {
-			// properties changed (ordering, columns, searching)
-			ajax = true;
-		}
-		
-		// Store the request for checking next time around
-		cacheLastRequest = $.extend( true, {}, request );
-
-		if ( ajax ) {
-			// Need data from the server
-			if ( requestStart < cacheLower ) {
-				requestStart = requestStart - (requestLength*(conf.pages-1));
-
-				if ( requestStart < 0 ) {
-					requestStart = 0;
-				}
-			}
-			
-			cacheLower = requestStart;
-			cacheUpper = requestStart + (requestLength * conf.pages);
-
-			request.start = requestStart;
-			request.length = requestLength*conf.pages;
-
-			// Provide the same `data` options as DataTables.
-			if ( $.isFunction ( conf.data ) ) {
-				// As a function it is executed with the data object as an arg
-				// for manipulation. If an object is returned, it is used as the
-				// data object to submit
-				var d = conf.data( request );
-				if ( d ) {
-					$.extend( request, d );
-				}
-			}
-			else if ( $.isPlainObject( conf.data ) ) {
-				// As an object, the data given extends the default
-				$.extend( request, conf.data );
-			}
 			settings.jqXHR = $.ajax( {
 				"type":     conf.method,
 				"url":      conf.url,
 				"data":     requestMapper(request),
 				"dataType": "json",
 				"cache":    false,
-				"success":  function ( json,textStatus, request) {
-					cacheLastJson = $.extend(true, {}, json);
-					if ( cacheLower != drawStart ) {
-						json.splice( 0, drawStart-cacheLower );
-					}
-					json.splice( requestLength, json.length );
-					
+				"success":  function ( json,textStatus, requestj) {
+					recordsTotal = requestj.getResponseHeader('recordsTotal');
+					recordsFiltered = requestj.getResponseHeader('recordsFiltered');
 					drawCallback({data: json,
-						recordsTotal: request.getResponseHeader('recordsTotal'),
-						recordsFiltered: request.getResponseHeader('recordsFiltered'),}
-					);
-
+						recordsTotal: recordsTotal,
+						recordsFiltered: recordsFiltered,
+					});
 				}
 			} );
-		}
-		else {
-			json = $.extend( true, {}, cacheLastJson );
-; // Update the echo for each response
-			json.splice( 0, requestStart-cacheLower );
-			json.splice( requestLength, json.length );
-			drawCallback({draw: request.draw,
-						  data: json,
-						  recordsTotal: request.getResponseHeader('recordsTotal'),
-						  recordsFiltered: request.getResponseHeader('recordsFiltered'),
-			});
-		}
-	}
+	
 };
 
 // Register an API method that will empty the pipelined data, forcing an Ajax
@@ -260,3 +176,4 @@ $.fn.dataTable.Api.register( 'clearPipeline()', function () {
 		settings.clearCache = true;
 	} );
 } );
+}
